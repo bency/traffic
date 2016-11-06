5292674f8891dad8a7b219b70405f86afaa4e355
bency <bency80097@gmail.com>
2016-11-06 22:23:44 +0800

    首頁加入 demo 用的假資料

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
+        <div class="col-md-8">
+            <img class="thumbnail center-block" src="//fakeimg.pl/600x300/">
+            <h3 class="text-center">新。考照制度提案書</h3>
+        </div>
+        <div class="col-md-4">
+            <img class="thumbnail center-block" src="//fakeimg.pl/300/">
+            <h3 class="text-center">機車路權與交通安全</h3>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-8">
+            <img class="thumbnail center-block" src="//fakeimg.pl/600x300/">
+            <h3 class="text-center">新。考照制度提案書</h3>
+        </div>
+        <div class="col-md-4">
+            <img class="thumbnail center-block" src="//fakeimg.pl/300/">
+            <h3 class="text-center">機車路權與交通安全</h3>
         </div>
     </div>
 </div>
