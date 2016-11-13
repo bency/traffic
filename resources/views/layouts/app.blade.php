3b81b2a2970a05e89379a13deb34d2ab297ee097
bency <bency80097@gmail.com>
2016-11-13 22:31:20 +0800

    移除登入/註冊功能

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,25 +50,6 @@
                     <li><a href="{{ url('/core') }}">中心思想</a></li>
                     <li><a href="{{ url('/safety') }}">交通安全 Q&A</a></li>
                 </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">登入</a></li>
-                        <li><a href="{{ url('/register') }}">註冊</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>登出</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
             </div>
         </div>
     </nav>
