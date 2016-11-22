01497d9d683d210f777ccc56ee768ae0050ad012
bency <bency80097@gmail.com>
2016-11-22 13:37:06 +0800

    js 加入 version controll

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,10 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="#purpose">提案目的</a></li>
-                    <li><a href="#observation">觀察現象</a></li>
-                    <li><a href="#differences">與他國差異</a></li>
-                    <li><a href="#propose">考照制度變革提案</a></li>
+                    <li><a class="menu-item" href="#purpose">提案目的</a></li>
+                    <li><a class="menu-item" href="#observation">觀察現象</a></li>
+                    <li><a class="menu-item" href="#differences">與他國差異</a></li>
+                    <li><a class="menu-item" href="#propose">考照制度變革提案</a></li>
                 </ul>
             </div>
         </div>
@@ -74,6 +74,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="{{ elixir('js/app.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
