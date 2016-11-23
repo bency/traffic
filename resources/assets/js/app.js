38dc6fabc808519ac4266ca28a4e063b827a24bb
bency <bency80097@gmail.com>
2016-11-24 00:36:15 +0800

    每 10 秒更新一次連署數量

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,12 @@
 $(document).on('click', ".menu-item", function () {
     $('#app-navbar-collapse').removeClass('in');
 });
+var updateContersigned = function () {
+    $.get('/countersigned').done(function (count) {
+        $('.countersigned-count').text(count);
+    });
+};
+$(document).on('ready', function(){
+    updateContersigned();
+    setInterval(updateContersigned, 10000);
+});
