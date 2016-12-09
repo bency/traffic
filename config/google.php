313380e9da94d618445f39caa44a78670f0a017f
bency <bency@pixnet.tw>
2016-12-10 01:15:00 +0800

    加入 google config

---
+++ config/google.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'client-secret-path' => __DIR__ . '/../resources/client-secret.json',
+    'app-name' => 'TTSA',
+    'scopes' => Google_Service_Sheets::SPREADSHEETS_READONLY,
+];
