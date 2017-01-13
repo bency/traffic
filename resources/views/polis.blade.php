10bdbb0db38f1d22823fb3cad6e4226392cc86dc
bency <bency@pixnet.tw>
2017-01-13 22:51:40 +0800

    修正 hash 為考照加嚴

--- resources/views/polis.blade.php
+++ resources/views/polis.blade.php
@@ -2,7 +2,7 @@
 @inject('agent', 'Jenssegers\Agent\Agent')
 @section('content')
     <div class="container">
-        <div class='polis' data-conversation_id='8yxfkvhrts'></div>
+        <div class='polis' data-conversation_id='6zatkfimer'></div>
         <script async='true' src='https://pol.is/embed.js'></script>
     </div>
 @endsection
