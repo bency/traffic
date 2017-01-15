4cd6ceb835d4ce765cea8dbe2960adfc72769b8c
bency <bency80097@gmail.com>
2017-01-15 14:26:52 +0800

    observation 使用 cache buster

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ elixir(function(mix) {
     mix.sass('app.scss');
     mix.scripts('observation.js');
     mix.scripts('app.js');
-    mix.version(['public/css/app.css', 'public/js/app.js']);
+    mix.version(['public/css/app.css', 'public/js/app.js', 'public/js/observation.js']);
     mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts/');
     mix.copy('node_modules/c3/c3.min.css', 'public/css/');
     mix.copy('node_modules/c3/c3.min.js', 'public/js/');
