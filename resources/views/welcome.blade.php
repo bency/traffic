52df7b11c8fd62d1e74369ba17ef372eb809aa46
bency <bency@pixnet.tw>
2017-01-23 02:28:32 +0800

    調整連署表單 label 寬度

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,19 +28,19 @@
                 <form class="form-horizontal" action="{{ route('submit.counter.sign') }}" method="post">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">真實姓名</label>
+                        <label for="name" class="col-sm-3 control-label">真實姓名</label>
                         <div class="col-sm-6">
                             <input type="text" class="form-control" id="name" name="name" required placeholder="姓名">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="email" class="col-sm-2 control-label">電子信箱</label>
+                        <label for="email" class="col-sm-3 control-label">電子信箱</label>
                         <div class="col-sm-6">
                             <input type="email" class="form-control" id="email" name="email" required placeholder="常用的電子信箱">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="phone" class="col-sm-2 control-label">聯絡電話</label>
+                        <label for="phone" class="col-sm-3 control-label">聯絡電話</label>
                         <div class="col-sm-6">
                             <input type="phone" class="form-control" id="phone" name="phone" required placeholder="手機或市話">
                         </div>
