190d9a3ff20bacb1c432deee817472e2a69f56be
bency <bency@pixnet.tw>
2017-01-23 02:37:08 +0800

    連署成功後顯示分享的按鈕

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,14 @@
 @section('content')
     <div class="container">
         @if(session('message'))
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.8&appId=529407913870999"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
             <div class="alert alert-{{ session('message')['type'] }}">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 {{ session('message')['text'] }}
+                @if('success' == session('message')['type'])
+                    <div class="fb-share-button" data-href="https://traffic.keeping.work/" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftraffic.keeping.work%2F&amp;src=sdkpreparse">按我分享此連署</a></div>
+                @endif
             </div>
         @endif
         <div class="row">
