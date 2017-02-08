0bba155d22718126c37c985f56f3ccef6cc2102f
bency <bency@pixnet.tw>
2017-02-09 01:44:43 +0800

    聯署時新增年份

--- app/Services/CountersignService.php
+++ app/Services/CountersignService.php
@@ -8,12 +8,13 @@ class CountersignService
 {
     public static function add(array $data)
     {
-        list($signed_at, $name, $phone, $email) = $data;
+        list($signed_at, $name, $phone, $email, $birth) = $data;
         $data = [
             'signed_at' => $signed_at,
             'name' => $name,
             'phone' => $phone,
             'email' => $email,
+            'birth' => $birth,
         ];
 
         return Countersign::create($data);
